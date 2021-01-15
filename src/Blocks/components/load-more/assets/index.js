ac6f6af65aea44bb192acd7a40143a66de7738e6
Goran Alković <goran.alkovic@infinum.hr>
2021-01-15 17:10:34 +0100

    make Load more have pagination

--- src/Blocks/components/load-more/assets/index.js
+++ src/Blocks/components/load-more/assets/index.js
@@ -1,7 +1,12 @@
 import domReady from '@wordpress/dom-ready';
 
+
+
 domReady(() => {
 	let elements = document.querySelectorAll('.load-more');
+
+	let start = 5;
+	let limit = 4;
 
 	for (let element of elements) {
 		const url = element.dataset.url;
@@ -13,13 +18,14 @@ domReady(() => {
 		const loadMoreContent = async (e) => {
 			e.preventDefault();
 
-			console.log(url);
-			console.log(containerClass);
-
-			let data = await fetch(url);
+			let data = await fetch(`${url}&_limit=${limit}&_start=${start}`);
 			let contents = await data.text();
 
+			contents = JSON.parse(contents);
+
 			container.innerHTML += contents;
+
+			start += limit;
 		}
 
 		button.addEventListener('click', loadMoreContent);
