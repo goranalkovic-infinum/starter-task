efdcc84af7e8d310fab9428389d5cb356ab89702
Goran Alković <goran.alkovic@infinum.hr>
2021-01-22 11:30:50 +0100

    update story to match component

--- src/Blocks/components/load-more/docs/story.js
+++ src/Blocks/components/load-more/docs/story.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Fragment } from '@wordpress/element';
 import readme from './readme.mdx';
 import manifest from './../manifest.json';
-import { ButtonEditor } from '../components/button-editor';
-import { ButtonOptions } from '../components/button-options';
-import { ButtonToolbar } from '../components/button-toolbar';
+import { LoadMoreEditor } from '../components/load-more-editor';
+import { LoadMoreOptions } from '../components/load-more-options';
 
 export default {
 	title: `Components/${manifest.title}`,
@@ -18,73 +16,9 @@ export default {
 const props = manifest.example.attributes;
 
 export const editor = () => (
-	<ButtonEditor {...props} />
+	<LoadMoreEditor {...props} />
 );
 
 export const options = () => (
-	<ButtonOptions {...props} />
-);
-
-export const toolbar = () => (
-	<ButtonToolbar {...props} />
-);
-
-export const size = () => (
-	<Fragment>
-		{manifest.options.sizes.map((values, index) => (
-			<Fragment key={index}>
-				<ButtonEditor
-					{...props}
-					buttonContent={values.label}
-					buttonSize={values.value}
-				/>
-				<br />
-			</Fragment>
-		))}
-	</Fragment>
-);
-
-export const width = () => (
-	<Fragment>
-		{manifest.options.widths.map((values, index) => (
-			<Fragment key={index}>
-				<ButtonEditor
-					{...props}
-					buttonContent={values.label}
-					buttonWidth={values.value}
-				/>
-				<br />
-			</Fragment>
-		))}
-	</Fragment>
-);
-
-export const align = () => (
-	<Fragment>
-		{manifest.options.aligns.map((values, index) => (
-			<Fragment key={index}>
-				<ButtonEditor
-					{...props}
-					buttonContent={values}
-					buttonAlign={values}
-				/>
-				<br />
-			</Fragment>
-		))}
-	</Fragment>
-);
-
-export const colors = () => (
-	<Fragment>
-		{manifest.options.colors.map((values, index) => (
-			<Fragment key={index}>
-				<ButtonEditor
-					{...props}
-					buttonContent={values}
-					buttonColor={values}
-				/>
-				<br />
-			</Fragment>
-		))}
-	</Fragment>
+	<LoadMoreOptions {...props} />
 );
