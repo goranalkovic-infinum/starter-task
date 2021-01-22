d6f06ffeb2239009359309d845e67b559fb6007e
Goran Alković <goran.alkovic@infinum.hr>
2021-01-22 11:30:10 +0100

    clean up index.js and use dynamic import

--- src/Blocks/components/load-more/assets/index.js
+++ src/Blocks/components/load-more/assets/index.js
@@ -1,31 +1,51 @@
-import domReady from '@wordpress/dom-ready';
+async function load() { 
+	let domReady = await import('@wordpress/dom-ready');
 
-domReady(() => {
-	let elements = document.querySelectorAll('.load-more');
-
-	let start = 5;
-	let limit = 4;
-
-	for (let element of elements) {
-		const url = element.dataset.url;
-		const containerClass = element.dataset.container;
-		const button = element.querySelector('button');
-
-		let container = document.querySelector(`.${containerClass}`);
-
-		const loadMoreContent = async (e) => {
-			e.preventDefault();
-
-			let data = await fetch(`${url}&_limit=${limit}&_start=${start}`);
-			let contents = await data.text();
-
-			contents = JSON.parse(contents);
-
-			container.innerHTML += contents;
-
-			start += limit;
+	domReady.default(() => {
+		const selector = '.js-load-more'
+		let elements = document.querySelectorAll(selector);
+	
+		for (let element of elements) {
+			const url = element.dataset.url;
+			const button = element.querySelector('button');
+			const pagination = element.dataset.paginated;
+			const startParam = element.dataset.startItemParam;
+			const limitParam = element.dataset.perPageParam;
+			const limit = parseInt(element.dataset.perPage);
+			let start = parseInt(element.dataset.startItem);
+	
+			console.log(element.dataset);
+	
+			let container = document.querySelector(`${selector}--container`);
+	
+			container.innerHTML = '';
+	
+			const loadMoreContent = async (e) => {
+				e.preventDefault();
+	
+				let urlToFetch = url;
+	
+				if (pagination) {
+					const startParams = url.includes('?') ? '&' : '?';
+					urlToFetch = `${url}${startParams}${startParam}=${start}&${limitParam}=${limit}`
+				}
+	
+				console.log(urlToFetch)
+	
+				let data = await fetch(urlToFetch);
+	
+				let contents = await data.text();
+	
+				contents = JSON.parse(contents);
+	
+				container.innerHTML += contents;
+	
+				start += limit;
+			}
+	
+			button.addEventListener('click', loadMoreContent);
 		}
+	});
+}
 
-		button.addEventListener('click', loadMoreContent);
-	}
-});
+load();
