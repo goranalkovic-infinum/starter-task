b31e24e9bc456872e2c9600edef0e73e4baf3aa9
Goran Alković <goran.alkovic@infinum.hr>
2021-01-25 14:54:43 +0100

    seperated logic into second file, dynamic imported

--- src/Blocks/components/load-more/assets/index.js
+++ src/Blocks/components/load-more/assets/index.js
@@ -1,46 +1,29 @@
 import domReady from '@wordpress/dom-ready';
 
-import manifest from '../manifest.json';
+domReady(async () => {
+	const selector = '.js-load-more'
+	let elements = document.querySelectorAll(selector);
 
-domReady( async () => {
-		const selector = '.js-load-more'
-		let elements = document.querySelectorAll(selector);
+	let { Loader } = await import('./loader');
 
+	if (elements.length) {
 		for (let element of elements) {
-			const url = element.dataset.url;
-			const button = element.querySelector(`${selector}-button`);
-			const pagination = element.dataset.paginated;
-			const startParam = element.dataset.startItemParam;
-			const limitParam = element.dataset.perPageParam;
-			const limit = parseInt(element.dataset.perPage);
-			let start = parseInt(element.dataset.startItem);
-
-			let container = document.querySelector(`${selector}-container`);
-
-			container.innerHTML = '';
-
-			const loadMoreContent = async (e) => {
-				e.preventDefault();
-
-				let urlToFetch = url;
-
-				if (pagination) {
-					const startParams = url.includes('?') ? '&' : '?';
-					urlToFetch = `${url}${startParams}${startParam}=${start}&${limitParam}=${limit}`
+			const loader = new Loader(
+				{
+					element,
+					url: element.dataset.url,
+					buttonSelector: `${selector}-button`,
+					containerSelector: `${selector}-container`,
+					pagination: element.dataset.paginated,
+					startParam: element.dataset.startItemParam,
+					limitParam: element.dataset.perPageParam,
+					start: parseInt(element.dataset.startItem),
+					limit: parseInt(element.dataset.perPage),
 				}
+			);
 
-				let data = await fetch(urlToFetch);
-
-				let contents = await data.text();
-
-				contents = JSON.parse(contents);
-
-				container.innerHTML += contents;
-
-				start += limit;
-			}
-
-			button.addEventListener('click', loadMoreContent);
+			loader.init();
 		}
 	}
+}
 );
---
+++ src/Blocks/components/load-more/assets/loader.js
@@ -0,0 +1,42 @@
+export class Loader {
+    constructor(options) {
+        this.element = options.element;
+        this.url = options.url;
+        this.buttonSelector = options.buttonSelector;
+        this.containerSelector = options.containerSelector;
+        this.pagination = options.pagination;
+        this.startParam = options.startParam;
+        this.limitParam = options.limitParam;
+        this.limit = options.limit;
+        this.start = options.start;
+    }
+
+    init() {
+        let button = this.element.querySelector(this.buttonSelector)
+        let container = this.element.querySelector(this.containerSelector);
+
+        container.innerHTML = '';
+
+        const loadMoreContent = async () => {
+            let urlToFetch = this.url;
+    
+            if (this.pagination) {
+                const startParams = this.url.includes('?') ? '&' : '?';
+    
+                urlToFetch = `${this.url}${startParams}${this.startParam}=${this.start}&${this.limitParam}=${this.limit}`
+            }
+    
+            const data = await fetch(urlToFetch);
+    
+            let contents = await data.text();
+    
+            contents = JSON.parse(contents);
+    
+            container.innerHTML += contents;
+    
+            this.start += this.limit;
+        };
+
+        button.addEventListener('click', loadMoreContent);
+    }
+}
