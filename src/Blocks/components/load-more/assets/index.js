e64622eaf05df94eaf57b7159126550c39846b54
Goran Alković <goran.alkovic@infinum.hr>
2021-01-25 14:56:51 +0100

    use forEach instead of for...of and async/await

--- src/Blocks/components/load-more/assets/index.js
+++ src/Blocks/components/load-more/assets/index.js
@@ -1,29 +1,27 @@
 import domReady from '@wordpress/dom-ready';
 
-domReady(async () => {
+domReady(() => {
 	const selector = '.js-load-more'
 	let elements = document.querySelectorAll(selector);
 
-	let { Loader } = await import('./loader');
-
 	if (elements.length) {
-		for (let element of elements) {
-			const loader = new Loader(
-				{
-					element,
-					url: element.dataset.url,
-					buttonSelector: `${selector}-button`,
-					containerSelector: `${selector}-container`,
-					pagination: element.dataset.paginated,
-					startParam: element.dataset.startItemParam,
-					limitParam: element.dataset.perPageParam,
-					start: parseInt(element.dataset.startItem),
-					limit: parseInt(element.dataset.perPage),
-				}
-			);
+		import('./loader').then(({ Loader }) =>
+			[...elements].forEach((element) => {
+				const loader = new Loader(
+					{
+						element,
+						url: element.dataset.url,
+						buttonSelector: `${selector}-button`,
+						containerSelector: `${selector}-container`,
+						pagination: element.dataset.paginated,
+						startParam: element.dataset.startItemParam,
+						limitParam: element.dataset.perPageParam,
+						start: parseInt(element.dataset.startItem),
+						limit: parseInt(element.dataset.perPage),
+					}
+				);
 
-			loader.init();
-		}
+				loader.init();
+			}));
 	}
-}
-);
+});
