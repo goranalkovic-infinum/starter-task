da7ecdaeea8605d462a8aa03eca49656ad521b94
Goran Alković <goran.alkovic@infinum.hr>
2021-01-25 16:20:07 +0100

    use load-more's options (with 1 additional field)

---
+++ src/Blocks/components/load-more/components/load-more-options.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { __, sprintf } from '@wordpress/i18n';
+import { Fragment } from '@wordpress/element';
+import { ToggleControl, TextControl, RangeControl } from '@wordpress/components';
+import { checkAttr } from '@eightshift/frontend-libs/scripts/helpers';
+import manifest from '../manifest.json';
+
+const { options, title } = manifest;
+
+export const LoadMoreOptions = (attributes) => {
+	const {
+		setAttributes,
+		componentName = manifest.componentName,
+		loadMoreShowControls = true,
+		label = title,
+		loadMoreUse = checkAttr('loadMoreUse', attributes, manifest, componentName),
+		loadMoreUrl = checkAttr('loadMoreUrl', attributes, manifest, componentName),
+		loadMoreUsePagination = checkAttr('loadMoreUsePagination', attributes, manifest, componentName),
+		loadMoreItemsPerPage = checkAttr('loadMoreItemsPerPage', attributes, manifest, componentName),
+		loadMoreItemsPerPageParameterName = checkAttr('loadMoreItemsPerPageParameterName', attributes, manifest, componentName),
+		loadMoreStartItem = checkAttr('loadMoreStartItem', attributes, manifest, componentName),
+		loadMoreStartItemParameterName = checkAttr('loadMoreStartItemParameterName', attributes, manifest, componentName),
+	} = attributes;
+
+
+	if (!loadMoreShowControls) {
+		return null;
+	}
+
+	return (
+		<Fragment>
+
+			{label &&
+				<h3 className={'options-label'}>
+					{label}
+				</h3>
+			}
+
+			<ToggleControl
+				label={sprintf(__('Use %s', 'Unicorns'), label)}
+				checked={loadMoreUse}
+				onChange={(value) => setAttributes({ [`loadMoreUse`]: value })}
+			/>
+
+
+			{loadMoreUse &&
+				<Fragment>
+
+				<TextControl
+					label="URL"
+					value={loadMoreUrl ?? ''}
+					onChange={(value) => setAttributes({ [`loadMoreUrl`]: value })}
+					/>
+
+				{loadMoreUsePagination && <hr />}
+
+				<ToggleControl
+					label={__('Pagination', 'Unicorns')}
+					checked={loadMoreUsePagination}
+					onChange={(value) => setAttributes({ [`loadMoreUsePagination`]: value })}
+				/>
+
+
+				{loadMoreUsePagination &&
+					<Fragment>
+						<RangeControl
+							label="Items per page"
+							value={loadMoreItemsPerPage ?? 0}
+							onChange={(value) => setAttributes({ [`loadMoreItemsPerPage`]: value })}
+							min={options.loadMoreItemsPerPage.min}
+							max={options.loadMoreItemsPerPage.max}
+							step={1}
+						/>
+						<TextControl
+							label="Items per page URL parameter"
+							value={loadMoreItemsPerPageParameterName ?? ''}
+							onChange={(value) => setAttributes({ [`loadMoreItemsPerPageParameterName`]: value })}
+						/>
+						<RangeControl
+							label="Starting item index"
+							value={loadMoreStartItem ?? 0}
+							onChange={(value) => setAttributes({ [`loadMoreStartItem`]: value })}
+							min={options.loadMoreStartItem.min}
+							max={options.loadMoreStartItem.max}
+							step={1}
+						/>
+						<TextControl
+							label="Starting item index URL parameter"
+							value={loadMoreStartItemParameterName ?? ''}
+							onChange={(value) => setAttributes({ [`loadMoreStartItemParameterName`]: value })}
+							/>
+						</Fragment>
+					}
+				</Fragment>
+			}
+		</Fragment>
+	);
+};
--- src/Blocks/custom/featured-posts/components/featured-posts-options.js
+++ src/Blocks/custom/featured-posts/components/featured-posts-options.js
@@ -1,143 +1,29 @@
 import React from 'react';
 import { __ } from '@wordpress/i18n';
-import { Fragment } from '@wordpress/element';
-import { useSelect } from '@wordpress/data';
-import { PanelBody, SelectControl, Spinner, RangeControl, ToggleControl, Icon } from '@wordpress/components';
-import { icons } from '@eightshift/frontend-libs/scripts/editor';
-import manifest from './../manifest.json';
+import { PanelBody } from '@wordpress/components';
+import { ToggleControl } from '@wordpress/components';
 
-const { attributes: reset, options } = manifest;
+import { LoadMoreOptions as LoadMoreOptionsComponent } from '../../../components/load-more/components/load-more-options';
+
 
 export const FeaturedPostsOptions = ({ attributes, setAttributes }) => {
 	const {
-		query: queryProps,
-		query: {
-			postType,
-			posts,
-		},
-		showItems,
-		itemsPerLine,
-		excludeCurrentPost,
+		blockName,
+		loadOnStart,
 	} = attributes;
 
-	// Fetch all post types.
-	// Filter allowed post types defined in the block manifest.
-	const postTypeOptions = useSelect((select) => {
-		const { getPostTypes } = select('core');
-
-		const items = getPostTypes() ?? [];
-
-		const data = items.filter((element) => manifest.allowed.postTypes.find((item) => element.slug === item)) ?? [];
-
-		return data.map((item) => {
-			return {
-				label: item.labels.name,
-				value: item.slug,
-				taxonomies: item.taxonomies,
-			};
-		}) ?? [];
-	});
-
-	// Fetch all posts based on the selected postType.
-	const postsOptions = useSelect((select) => {
-		const { getEntityRecords } = select('core');
-
-		const termsList = getEntityRecords(
-			'postType',
-			postType,
-			{
-				per_page: -1,
-			}
-		) ?? [];
-
-		return [
-			{
-				label: 'None',
-				value: '',
-			},
-			...termsList.map((item) => {
-				return {
-					label: item.title.rendered,
-					value: item.id,
-				};
-			}),
-		];
-	});
-
 	return (
-		<PanelBody title={__('Featured Post', 'Unicorns')}>
-
-			{postTypeOptions[0] ?
-				<SelectControl
-					label={__('Post Type', 'Unicorns')}
-					value={postType}
-					options={postTypeOptions}
-					onChange={(value) => {
-						setAttributes({
-							query: {
-								postType: value,
-								posts: [],
-							},
-						});
-					}}
-				/> :
-				<Spinner />
-			}
-
-			{(postTypeOptions[0] && posts) ?
-				<SelectControl
-					label={__('Posts Items', 'Unicorns')}
-					value={posts}
-					multiple
-					options={postsOptions}
-					onChange={(value) => {
-						setAttributes({
-							query: {
-								...queryProps,
-								posts: value[0] ? value : [],
-							},
-						});
-					}}
-				/> :
-				<Spinner />
-			}
-
-			<RangeControl
-				label={
-					<Fragment>
-						<Icon icon={icons.width} />
-						{__('Items per one row', 'Unicorns')}
-					</Fragment>
-				}
-				help={__('Option to change the number of items showed in one row.', 'Unicorns')}
-				allowReset={true}
-				value={itemsPerLine}
-				onChange={(value) => setAttributes({ itemsPerLine: value })}
-				min={options.itemsPerLine.min}
-				max={options.itemsPerLine.max}
-				step={options.itemsPerLine.step}
-				resetFallbackValue={reset.itemsPerLine.default}
-			/>
-
-			<RangeControl
-				label={
-					<Fragment>
-						<Icon icon={icons.width} />
-						{__('Show items', 'Unicorns')}
-					</Fragment>
-				}
-				help={__('Option to change the number of items to show in total.', 'Unicorns')}
-				allowReset={true}
-				value={showItems}
-				onChange={(value) => setAttributes({ showItems: value })}
-				step={1}
-			/>
+		<PanelBody title={__('Featured Posts', 'Unicorns')}>
 
 			<ToggleControl
-				label={__('Exclude current post', 'Unicorns')}
-				checked={excludeCurrentPost}
-				help={__('This options can only be used in post single pages.', 'Unicorns')}
-				onChange={(value) => setAttributes({ excludeCurrentPost: value })}
+				label={__('Initial item load', 'Unicorns')}
+				checked={loadOnStart}
+				onChange={(value) => setAttributes({ loadOnStart: value })}
+			/>
+
+			<LoadMoreOptionsComponent
+				{...attributes}
+				setAttributes={setAttributes}
 			/>
 
 		</PanelBody>
