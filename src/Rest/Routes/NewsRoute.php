182754b08c485232538856a97732be2c8cfe9b55
Goran Alković <goran.alkovic@infinum.hr>
2021-01-26 13:57:38 +0100

    refactor NewsRoute to use new class

--- src/Rest/Routes/NewsRoute.php
+++ src/Rest/Routes/NewsRoute.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 namespace Unicorns\Rest\Routes;
 
 use Unicorns\Config\Config;
-use UnicornsVendor\EightshiftLibs\Helpers\Components;
 use UnicornsVendor\EightshiftLibs\Rest\Routes\AbstractRoute;
 use UnicornsVendor\EightshiftLibs\Rest\CallableRouteInterface;
 
@@ -20,6 +19,7 @@ use UnicornsVendor\EightshiftLibs\Rest\CallableRouteInterface;
  */
 class NewsRoute extends AbstractRoute implements CallableRouteInterface
 {
+	private const ROUTE_NAME = '/news';
 
 	/**
 	 * Method that returns project Route namespace.
@@ -48,7 +48,7 @@ class NewsRoute extends AbstractRoute implements CallableRouteInterface
 	 */
 	protected function getRouteName(): string
 	{
-		return '/news';
+		return self::ROUTE_NAME;
 	}
 
 	/**
@@ -66,10 +66,9 @@ class NewsRoute extends AbstractRoute implements CallableRouteInterface
 	}
 
 	/**
-	 * Method that returns rest response
+	 * Callback method for NewsRoute.
 	 *
-	 * @param \WP_REST_Request $request Data got from endpoint url.
-	 *
+	 * @param \WP_REST_Request $data Data got from endpoint url.
 	 * @return \WP_REST_Response|mixed If response generated an error, WP_Error, if response
 	 *                                is already an instance, WP_HTTP_Response, otherwise
 	 *                                returns a new WP_REST_Response instance.
@@ -78,65 +77,10 @@ class NewsRoute extends AbstractRoute implements CallableRouteInterface
 	{
 		$params = $data->get_params();
 
-		$returnHtml = false;
+		$api = new NewsApi();
 
-		if (isset($params)) {
-			foreach ($params as $name => $value) {
-				if ($name != '_limit' && $name != '_start') {
-					unset($params[$name]);
-				}
-				if ($name == 'html') {
-					$returnHtml = true;
-				}
-			}
-		}
+		$data = $api->getData($params);
 
-		$parsedParams = "";
-
-		if (isset($params) && sizeof($params) > 0) {
-			$parsedParams = '?' . http_build_query($params);
-		}
-
-		$url = "https://test.spaceflightnewsapi.net/api/v2/articles$parsedParams";
-		$remoteResponse = \wp_remote_get($url);
-
-		if (is_array($remoteResponse)) {
-			$body = json_decode($remoteResponse['body'], true);
-		}
-
-		if (!isset($body)) {
-			return new \WP_Error('no_news', 'there are no news available', ['status' => 404]);
-		}
-
-		if ($returnHtml) {
-			$outputHtml = '';
-
-			foreach ($body as $post) {
-				$postImage = $post['imageUrl'];
-
-				$postDate = date_create($post['publishedAt']);
-
-				$postTitle = $post['title'];
-
-				$postExcerpt = $post['summary'];
-
-				$postCardProps = [
-					'imageUrl' => wp_kses_post($postImage),
-					'imageUse' => $postImage ?? true,
-					'dateContent' => date_format($postDate, 'j.m.Y @ G:i'),
-					'headingContent' => wp_kses_post($postTitle),
-					'excerptContent' => wp_kses_post($postExcerpt),
-					'tagsContent' => '<li>Prvo</li><li>Drugo</li><li>Trece</li>'
-				];
-
-				$outputHtml .= '<div class="block-featured-posts__item">';
-				$outputHtml .= Components::render('post-card', $postCardProps);
-				$outputHtml .= '</div>';
-			}
-
-			return \rest_ensure_response($outputHtml);
-		}
-
-		return \rest_ensure_response($body);
+		return $data;
 	}
 }
