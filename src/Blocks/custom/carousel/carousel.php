f10989a97605bddcb77c7fc1c3eeb63d910c1c18
Goran Alković <goran.alkovic@infinum.hr>
2021-01-26 12:07:23 +0100

    add non-bem classes to enable styling

--- src/Blocks/custom/carousel/carousel.php
+++ src/Blocks/custom/carousel/carousel.php
@@ -23,22 +23,25 @@ $carouselClass = Components::classnames([
 
 ?>
 
-<div class="<?php echo esc_attr($carouselClass); ?>" data-swiper-loop="<?php echo esc_attr($isLoop); ?>" data-show-items="<?php echo esc_attr($showItems); ?>">
+<div class="<?php echo esc_attr($carouselClass); ?>" 
+	data-swiper-loop="<?php echo esc_attr($isLoop); ?>" 
+	data-show-items="<?php echo esc_attr($showItems); ?>">
 
 	<div class="<?php echo esc_attr('swiper-wrapper'); ?>">
 		<?php
-		$innerBlockContent = str_replace( "{$blockClass}__item", "{$blockJsClass}-item", $innerBlockContent);
+		$innerBlockContent = str_replace( "{$blockClass}__item", "{$blockClass}__item {$blockJsClass}-item", $innerBlockContent);
+		$innerBlockContent = str_replace( "{$blockJsClass}-item-inner", "{$blockClass}__item-inner {$blockJsClass}-item-inner", $innerBlockContent);
 		echo wp_kses_post($innerBlockContent);
 		?>
 	</div>
 
-	<div class="<?php echo esc_attr("{$blockJsClass}-control-container"); ?>">
-		<button class="<?php echo esc_attr("{$blockJsClass}-icon"); ?> <?php echo esc_attr("{$blockJsClass}-icon-prev"); ?>">
+	<div class="<?php echo esc_attr("{$blockClass}__control-container {$blockJsClass}-control-container"); ?>">
+		<button class="<?php echo esc_attr("{$blockClass}__icon {$blockJsClass}-icon"); ?> <?php echo esc_attr("{$blockClass}__icon--prev {$blockJsClass}-icon-prev"); ?>">
 			<?php echo $manifest['resources']['arrowLeft']; ?>
 		</button>
-		<button class="<?php echo esc_attr("{$blockJsClass}-icon"); ?> <?php echo esc_attr("{$blockJsClass}-icon-next"); ?>">
+		<button class="<?php echo esc_attr("{$blockClass}__icon {$blockJsClass}-icon"); ?> <?php echo esc_attr("{$blockClass}__icon--next {$blockJsClass}-icon-next"); ?>">
 			<?php echo $manifest['resources']['arrowRight']; ?>
 		</button>
-		<span class="<?php echo esc_attr("{$blockJsClass}-pagination"); ?>"></span>
+		<span class="<?php echo esc_attr("{$blockClass}__pagination {$blockJsClass}-pagination"); ?>"></span>
 	</div>
 </div>
